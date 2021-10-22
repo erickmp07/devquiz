86a375153e513c64cf6e2e1b1de9b22d97cc979d
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-10-22 18:57:39 -0300

    Set user's name and avatar

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,4 +1,5 @@
 import 'package:DevQuiz/core/app_gradients.dart';
+import 'package:DevQuiz/core/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
@@ -6,6 +7,31 @@ class AppBarWidget extends PreferredSize {
       : super(
             preferredSize: Size.fromHeight(250),
             child: Container(
+              height: 250,
               decoration: BoxDecoration(gradient: AppGradients.linear),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text.rich(TextSpan(
+                        text: "Hello, ",
+                        style: AppTextStyles.title,
+                        children: [
+                          TextSpan(
+                              text: "Erick", style: AppTextStyles.titleBold)
+                        ])),
+                    Container(
+                      width: 58,
+                      height: 58,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://avatars.githubusercontent.com/u/16216886?v=4"))),
+                    )
+                  ],
+                ),
+              ),
             ));
 }
