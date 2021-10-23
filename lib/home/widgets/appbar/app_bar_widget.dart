2678cd5e338ebfdac063825b7b9c6263e1033cdb
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-10-22 23:59:53 -0300

    Add the score card widget to the appbar widget

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:DevQuiz/core/app_gradients.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
@@ -11,25 +12,31 @@ class AppBarWidget extends PreferredSize {
               decoration: BoxDecoration(gradient: AppGradients.linear),
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Text.rich(TextSpan(
-                        text: "Hello, ",
-                        style: AppTextStyles.title,
-                        children: [
-                          TextSpan(
-                              text: "Erick", style: AppTextStyles.titleBold)
-                        ])),
-                    Container(
-                      width: 58,
-                      height: 58,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                              image: NetworkImage(
-                                  "https://avatars.githubusercontent.com/u/16216886?v=4"))),
-                    )
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text.rich(TextSpan(
+                            text: "Hello, ",
+                            style: AppTextStyles.title,
+                            children: [
+                              TextSpan(
+                                  text: "Erick", style: AppTextStyles.titleBold)
+                            ])),
+                        Container(
+                          width: 58,
+                          height: 58,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://avatars.githubusercontent.com/u/16216886?v=4"))),
+                        )
+                      ],
+                    ),
+                    ScoreCardWidget()
                   ],
                 ),
               ),
