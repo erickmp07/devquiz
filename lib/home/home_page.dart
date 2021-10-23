75330af390ae6ec2438fd1c6aacf0bd9df1ad8d0
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-10-23 02:00:19 -0300

    Add level buttons to the home page

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
-import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,6 +14,26 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            LevelButtonWidget(
+              label: "Easy",
+            ),
+            LevelButtonWidget(
+              label: "Medium",
+            ),
+            LevelButtonWidget(
+              label: "Hard",
+            ),
+            LevelButtonWidget(
+              label: "Expert",
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
