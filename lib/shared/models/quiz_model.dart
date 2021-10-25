717cfe45fa4ab0b39e4eadf8c33002afd3aeab8e
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-10-25 00:35:08 -0300

    Implement JSON serialization methods

--- lib/shared/models/quiz_model.dart
+++ lib/shared/models/quiz_model.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:DevQuiz/shared/enums/extensions/level_extension.dart';
+import 'package:DevQuiz/shared/enums/extensions/level_string_extension.dart';
 import 'package:DevQuiz/shared/enums/level_enum.dart';
 import 'package:DevQuiz/shared/models/question_model.dart';
 
@@ -14,4 +18,29 @@ class QuizModel {
       this.questionsAnswered = 0,
       required this.image,
       required this.level});
+
+  Map<String, dynamic> toMap() {
+    return {
+      "title": title,
+      "questions": questions.map((question) => question.toMap()).toList(),
+      "questionsAnswered": questionsAnswered,
+      "image": image,
+      "level": level.parse
+    };
+  }
+
+  factory QuizModel.fromMap(Map<String, dynamic> map) {
+    return QuizModel(
+        title: map["title"],
+        questions: List<QuestionModel>.from(map["questions"]
+            ?.map((question) => QuestionModel.fromMap(question))),
+        questionsAnswered: map["questionsAnswered"],
+        image: map["image"],
+        level: map["level"].toString().levelParse);
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory QuizModel.fromJson(String source) =>
+      QuizModel.fromMap(json.decode(source));
 }
