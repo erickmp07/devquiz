c017f958496f7076c656e6e822b5cee541050260
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-10-25 01:40:37 -0300

    Get data from the repository

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/foundation.dart';
-import 'package:DevQuiz/core/app_images.dart';
+import 'package:DevQuiz/home/home_repository.dart';
 import 'package:DevQuiz/home/home_state_enum.dart';
-import 'package:DevQuiz/shared/enums/level_enum.dart';
-import 'package:DevQuiz/shared/models/answer_model.dart';
-import 'package:DevQuiz/shared/models/question_model.dart';
 import 'package:DevQuiz/shared/models/quiz_model.dart';
 import 'package:DevQuiz/shared/models/user_model.dart';
 
@@ -16,12 +13,12 @@ class HomeController {
   UserModel? user;
   List<QuizModel>? quizzes;
 
+  final homeRepository = HomeRepository();
+
   void getUser() async {
     state = HomeState.loading;
 
-    user = UserModel(
-        name: "Erick",
-        photoUrl: "https://avatars.githubusercontent.com/u/16216886?v=4");
+    user = await homeRepository.getUser();
 
     state = HomeState.success;
   }
@@ -29,31 +26,7 @@ class HomeController {
   void getQuizzes() async {
     state = HomeState.loading;
 
-    quizzes = [
-      QuizModel(
-          title: "State Management",
-          questions: [
-            QuestionModel(
-                title: "What does Flutter do in its entirety?",
-                answers: [
-                  AnswerModel(
-                      title:
-                          "Enables the creation of natively compiled applications"),
-                  AnswerModel(
-                      title:
-                          "Enables the creation of natively compiled applications"),
-                  AnswerModel(
-                      title:
-                          "Enables the creation of natively compiled applications"),
-                  AnswerModel(
-                      title:
-                          "Enables the creation of natively compiled applications",
-                      isRight: true),
-                ])
-          ],
-          image: AppImages.blocks,
-          level: Level.easy)
-    ];
+    quizzes = await homeRepository.getQuizzes();
 
     state = HomeState.success;
   }
