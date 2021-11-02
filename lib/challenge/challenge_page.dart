8bffb781a9db16bc43b0abe05a9beab5a4387a70
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-11-02 14:16:54 -0300

    Create button widget to the challenges

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,3 +1,4 @@
+import 'package:DevQuiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:DevQuiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
@@ -19,6 +20,30 @@ class _ChallengePageState extends State<ChallengePage> {
       ),
       body: QuizWidget(
         title: "What does Flutter do in its entirety?",
+      ),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                  child: NextButtonWidget.white(
+                label: "Skip",
+                onTap: () {},
+              )),
+              SizedBox(
+                width: 7,
+              ),
+              Expanded(
+                  child: NextButtonWidget.green(
+                label: "Confirm",
+                onTap: () {},
+              ))
+            ],
+          ),
+        ),
       ),
     );
   }
